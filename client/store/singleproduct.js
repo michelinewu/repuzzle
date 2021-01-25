30fd0c75dbca2ad0d49b61f9f173e11b15f293ce
Micheline Wu <micheline.wu@gmail.com>
2021-01-24 22:42:27 -0500

    Fix: SingleProduct thunk removed comments.

--- client/store/singleproduct.js
+++ client/store/singleproduct.js
@@ -19,13 +19,9 @@ export const setSingleProduct = product => {
  * THUNK CREATORS
  */
 export const fetchSingleProduct = productId => {
-  console.log('fetchSingleProduct thunk')
   return async dispatch => {
-    console.log('try dispatching')
     try {
-      console.log('in try')
       const {data} = await axios.get(`/api/products/${productId}`)
-      console.log('fetchSingleProduct Thunk ', data)
       dispatch(setSingleProduct(data))
     } catch (err) {
       console.log(err)
